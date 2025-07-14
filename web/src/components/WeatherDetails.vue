9ff036a7ffecdd7cb5055e6b2ae48f20b0737bdf
Tong XUE <xtstella@hotmail.com>
2025-07-14 17:50:36 +0200

    fix: simplify code

--- web/src/components/WeatherDetails.vue
+++ web/src/components/WeatherDetails.vue
@@ -1,68 +1,17 @@
 <template>
   <div class="row row-cols-2 g-3">
-    <div v-if="forecast[0]" class="col">
+    <div v-for="(item, index) in forecast" :key="index" class="col">
       <div class="card text-center p-3">
         <div class="mt-2">Probability of rain</div>
-        <div class="fs-3 fw-bold">{{ forecast[0].probarain }} mm</div>
+        <div class="fs-3 fw-bold">{{ item.probarain }} mm</div>
         <div class="d-flex justify-content-around mt-2">
           <div>
             <small>Min</small>
-            <div>{{ forecast[0].tmin }}°C</div>
+            <div>{{ item.tmin }}°C</div>
           </div>
           <div>
             <small>Max</small>
-            <div>{{ forecast[0].tmax }}°C</div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div v-if="forecast[1]" class="col">
-      <div class="card text-center p-3">
-        <div class="mt-2">Probability of rain</div>
-        <div class="fs-3 fw-bold">{{ forecast[1].probarain }} mm</div>
-        <div class="d-flex justify-content-around mt-2">
-          <div>
-            <small>Min</small>
-            <div>{{ forecast[1].tmin }}°C</div>
-          </div>
-          <div>
-            <small>Max</small>
-            <div>{{ forecast[1].tmax }}°C</div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div v-if="forecast[2]" class="col">
-      <div class="card text-center p-3">
-        <div class="mt-2">Probability of rain</div>
-        <div class="fs-3 fw-bold">{{ forecast[2].probarain }} mm</div>
-        <div class="d-flex justify-content-around mt-2">
-          <div>
-            <small>Min</small>
-            <div>{{ forecast[2].tmin }}°C</div>
-          </div>
-          <div>
-            <small>Max</small>
-            <div>{{ forecast[2].tmax }}°C</div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div v-if="forecast[3]" class="col">
-      <div class="card text-center p-3">
-        <div class="mt-2">Probability of rain</div>
-        <div class="fs-3 fw-bold">{{ forecast[3].probarain }} mm</div>
-        <div class="d-flex justify-content-around mt-2">
-          <div>
-            <small>Min</small>
-            <div>{{ forecast[3].tmin }}°C</div>
-          </div>
-          <div>
-            <small>Max</small>
-            <div>{{ forecast[3].tmax }}°C</div>
+            <div>{{ item.tmax }}°C</div>
           </div>
         </div>
       </div>
